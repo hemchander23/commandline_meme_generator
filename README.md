ff2dc7cac6031d39043ce1957d3698f5299fc08c
Hem <hemchander.23@gmail.com>
2019-08-04 20:07:11 +0530

    Add impact font

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Command Line tool to generate memes from image templates and GIPHY
 Contributions are welcome
+
+mkmeme create-meme  "text1|10,300:text2|15,10" -i "sample.jpg"
--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const program = require('commander');
 const Jimp = require('jimp');
+
 program
   .command('create-meme <text>')
   .option('-i, --image [imagepath]', 'Base Image')
   .action(function (text, cmd) {
-    console.log(text);
-    console.log(cmd.image);
-    
-    Jimp.read(cmd.image, (err, duck) => {
+    // console.log(text);
+    // console.log(cmd.image);
+    Jimp.read(cmd.image, (err, img) => {
       if (err) throw err;
-      Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(font => {
-        duck.print(font, 10, 10, text, 800);
-        duck.write('duck_w.jpg'); // save
+      img.resize(Jimp.AUTO,750);
+      Jimp.loadFont("./font/impact-yellow/72/font.fnt").then(font => {
+        img.print(font, 10, 10, {
+          text: text, 
+          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
+        }, img.bitmap.width - 10);
+        img.write('duck_w.jpg'); // save
       });
     });
   });
