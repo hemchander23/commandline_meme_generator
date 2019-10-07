cd25d190e82dc2cfad5509072c0c5ea9b0c9ea62
Pooja Khandhadia <pooja.khandhadia.pk@gmail.com>
2019-10-07 14:59:51 +0000

    Add default value for scale

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ program
   .command('create-meme <text>')
   .option('-i, --imagein [imagepath]', 'Base Image')
   .option('-o, --imageout [imagepath]', 'Output Image')
-  .option('-s, --scale [number]', 'Provide a value from 0 to 1 to adjust the image scale')
+  .option('-s, --scale [number]', 'Provide a value from 0 to 1 to adjust the image scale', 1.0)
   .option('-p, --position [number]', 'Provide a value from 0 to 1 to adjust the image scale')
   .option('-a, --align [L,C,R]', 'Provide a alignment value. Default is center (C)')
   .action(function (text, cmd) {
@@ -43,7 +43,7 @@ program
   function writeTextToMeme(img,text,y,cmd,align){
     Jimp.loadFont("./font/impact-yellow/72/font.fnt").then(font => {
       img.print(font, 10, y, {
-        text: text, 
+        text: text,
         alignmentX: align
       }, img.bitmap.width - 50);
       img.write(cmd.imageout);
