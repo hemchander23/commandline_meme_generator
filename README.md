8867298e1bcd6a77fa1773581a4253d325378cae
Hem <hemchander.23@gmail.com>
2019-10-07 19:16:38 +0530

    Init

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-## Command Line tool to generate memes from image templates and GIPHY
-Contributions are welcome
+## Light-weight CLI to generate meme from image
 
-mkmeme create-meme  "text1|10,300:text2|15,10" -i "sample.jpg"
+Test:
+
+`mkmeme create-meme  "New Feature|Developers" -i "sample/input.jpg" -o 'sample/output.jpg' -s 1.6 -p "20|500" -a 'C'`
+
+Options:
+
+'mkmeme create-meme <text>'
+  - '-i, --imagein [imagepath]' -> 'Base Image'
+  - '-o, --imageout [imagepath]' -> 'Output Image'
+ - '-s, --scale [number]' -> 'Provide a value from 0 to 1 to adjust the image scale'
+ -  '-p, --position [number]' -> 'Provide a value from 0 to 1 to adjust the image scale'
+- '-a, --align [L,C,R]' -> 'Provide a alignment value. Default is center (C)'
+
+
+Before:
+
+![](sample/input.jpg)
+
+After:
+
+![](sample/output.jpg)
+
+To Contribute:
+
+1. Fork this repository and `cd` into the directory
+2. Do `npm i -g`
+3. Run the test command to generate a sample MEME
+4. Create a feature branch and start making changes
+5. Once you are done, raise a PR. 
+6. Changes (if any) will be suggested. Kindly make the necessary changes.
+7. Upon approval, PR will be merged !!! 🎉
+
+Contributions are welcome !
--- app.js
+++ app.js
@@ -1,23 +1,53 @@
+#!/usr/bin/env node
+
 const program = require('commander');
 const Jimp = require('jimp');
 
 program
   .command('create-meme <text>')
-  .option('-i, --image [imagepath]', 'Base Image')
+  .option('-i, --imagein [imagepath]', 'Base Image')
+  .option('-o, --imageout [imagepath]', 'Output Image')
+  .option('-s, --scale [number]', 'Provide a value from 0 to 1 to adjust the image scale')
+  .option('-p, --position [number]', 'Provide a value from 0 to 1 to adjust the image scale')
+  .option('-a, --align [L,C,R]', 'Provide a alignment value. Default is center (C)')
   .action(function (text, cmd) {
-    // console.log(text);
-    // console.log(cmd.image);
-    Jimp.read(cmd.image, (err, img) => {
+    Jimp.read(cmd.imagein, (err, img) => {
       if (err) throw err;
-      img.resize(Jimp.AUTO,750);
-      Jimp.loadFont("./font/impact-yellow/72/font.fnt").then(font => {
-        img.print(font, 10, 10, {
-          text: text, 
-          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
-        }, img.bitmap.width - 10);
-        img.write('duck_w.jpg'); // save
-      });
+      img.scale(parseFloat(cmd.scale));
+      switch(cmd.align){
+        case 'L':
+          align = 1
+          break;
+        case 'R':
+          align = 4;
+          break;
+        case 'C':
+        default:
+          align = 2;
+          break;
+      }
+      if(cmd.position){
+        let y_arr = String(cmd.position).split("|");
+        let tx_arr = String(text).split("|");
+        if(tx_arr.length > y_arr.length)
+          console.error("More text to position. Make sure the number of text parts match the position blocks defined");
+        y_arr.forEach(function(v,k){
+          writeTextToMeme(img,String(tx_arr[k]),parseInt(v),cmd,align);
+        })
+      }else{
+        writeTextToMeme(img,text,20,cmd,align);
+      }
     });
   });
+
+  function writeTextToMeme(img,text,y,cmd,align){
+    Jimp.loadFont("./font/impact-yellow/72/font.fnt").then(font => {
+      img.print(font, 10, y, {
+        text: text, 
+        alignmentX: align
+      }, img.bitmap.width - 50);
+      img.write(cmd.imageout);
+    });
+  }
 
 program.parse(process.argv);
