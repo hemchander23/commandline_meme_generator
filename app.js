e2ff1ced15fbdc3d30ab2d26b9a3c8339e91b133
Hem <hemchander.23@gmail.com>
2019-10-08 11:31:18 +0530

    JIMP native support for URL download JIMP already supports downloading image via URL. !!!

--- app.js
+++ app.js
@@ -17,23 +17,11 @@ program
     if (cmd.imagein) {
       Jimp.read(cmd.imagein, (err, img) => {
         if (err) {
-          console.error("Argument error");
+          console.error("An Error Occurred",err);
+          return;
         }
         //Download image if its a public URL
-        if (cmd.imagein) {
-          let image_in = cmd.imagein;
-          if (validateUrl(image_in)) {
-            //Download the image
-            download(image_in, 'image.png', function (mimety) {
-              // console.log(mimety);
-              if (['png', 'jpg', 'jpeg', 'tiff', 'gif', 'bmp'].contains(mimety)) {
-                cmd.imagein = 'image.png';
-              } else {
-                throw "Unsupported image format !";
-              }
-            });
-          }
-        } else {
+        if (!cmd.imagein) {
           throw "Cannot proceed without input image";
         }
         //Scale the image
@@ -77,21 +65,6 @@ function writeTextToMeme(img, text, y, cmd, align) {
     }, img.bitmap.width - 50);
     img.write(cmd.imageout);
   });
-}
-
-function download(uri, filename, callback) {
-  request.head(uri, function (err, res, body) {
-    // console.log('content-type:', res.headers['content-type']);
-    // console.log('content-length:', res.headers['content-length']);
-    const mimety = mime.getExtension(res.headers['content-type']);
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', function () {
-      callback(mimety);
-    });
-  });
-};
-
-function validateUrl(value) {
-  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
 }
 
 Array.prototype.contains = function (element) {
